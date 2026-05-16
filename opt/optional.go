2c2be7e7615d589d752cfd004a8907d8d3ca1eb9
Alan Lima <alanlimanvc@gmail.com>
2026-05-15 21:12:55 -0300

    feat(opt): add IsZero, Or and Interface methods

--- opt/optional.go
+++ opt/optional.go
@@ -1,51 +1,82 @@
 // Package opt implements a optional type, it may or may not contain
-// a value. It can also be marshalled and unmarshalled with JSON,
-// using the underlying marshelers, if such exist.
+// a value.
 package opt
 
 import "fmt"
 
 // Opt is an optional type, it may or may not contatin a value of a
-// certain type, given by its type parameter. Its zero value is
-// called None, when printed: <none>, it indicates the absence of
-// value.
+// certain type, given by its type parameter. Its zero value
+// indicates the absence of value, when printed: <none>.
 //
-// As it stands, an arbitrary [Opt] can be marshalled and
-// unmarshalled with JSON, with JSON's null being considered the zero
-// value, regardless of the type param.
+// An arbitrary [Opt] can be marshalled and unmarshalled with JSON,
+// with JSON's null being considered the zero value, regardless of
+// the type param. If available, it will try to use [json.Marshaler]
+// and [json.Unmarshaler] for the type param.
+//
+// An arbitrary [Opt] can be valued and scanned with SQL, with SQL's
+// null being considered the zero value, regardless of the type
+// param. If available, it will try to use [driver.Valuer] and
+// [sql.Scanner] for the type param.
 type Opt[T any] struct {
 	val  T
 	some bool
 }
 
-// Some creates a new Opt value with a value.
+// Some creates a new optional with the given value.
 func Some[T any](val T) Opt[T] {
 	return Opt[T]{val, true}
 }
 
-// None creates a new Opt value with no value, ie, None.
+// None creates a new optional with no value.
 func None[T any]() Opt[T] {
 	return Opt[T]{}
 }
 
-// Unwrap unpacks the Opt struct and returns its components, a common
-// assertion should be done, it may be done in the following way:
+// Unwrap unpacks the Opt and returns its components, a common
+// assertion may be done:
 //
 //	val, ok := opt.Unwrap()
 //	if !ok {
 //		// handle noneness
 //	}
 //
-// Since this a non-build-in function, the ok return cannot be
-// ommited.
-func (o Opt[T]) Unwrap() (T, bool) {
-	return o.val, o.some
+// If ok is false, val will be the zero value of T.
+func (o Opt[T]) Unwrap() (val T, ok bool) {
+	if o.some {
+		return o.val, true
+	}
+	return
 }
 
-// Present tells whether the optional has a value without unpacking
-// it.
-func (o* Opt[T]) Present() bool {
+// Present reports whether the optional has a value.
+func (o Opt[T]) Present() bool {
 	return o.some
+}
+
+// IsZero reports whether the optional does not have a value.
+func (o Opt[T]) IsZero() bool {
+	return !o.some
+}
+
+// Or returns the value of the optional if it is present, otherwise
+// it returns the given default value.
+func (o Opt[T]) Or(def T) T {
+	if o.some {
+		return o.val
+	}
+	return def
+}
+
+// Interface returns the value of the optional as an empty interface,
+// if it is present, otherwise it returns nil.
+//
+// If the optional is of an interface type, the nil by absence and
+// a present nil are indistinguishable.
+func (o Opt[T]) Interface() any {
+	if o.some {
+		return o.val
+	}
+	return nil
 }
 
 // String implements the [fmt.Stringer] interface, it returns the
